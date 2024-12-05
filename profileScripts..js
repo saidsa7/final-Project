a5ebbcb62842079db9568786fa0d17bf7fd46b78
said <bourjilasaid@gmail.com>
2024-12-05 10:30:42 +0100

    in process

--- profileScripts..js
+++ profileScripts..js
@@ -1,1 +1,14 @@
 setupUi();
+
+function profileClicked() {
+  user = getCurrentUser();
+  if (user != null) {
+    document.getElementById("profile-email").innerHTML = user.email;
+    document.getElementById("profile-name").innerHTML = user.name;
+    document.getElementById("profile-username").innerHTML = user.username;
+    document.getElementById("profile-posts-count").innerHTML = user.posts_count;
+    document.getElementById("profile-comments-count").innerHTML =
+      user.comments_count;
+    console.log(user);
+  }
+}
